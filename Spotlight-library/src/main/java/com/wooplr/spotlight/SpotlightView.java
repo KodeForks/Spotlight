7ee84be59db338290f622875d106152a0c58c9fa
Next Mobile Development <them1kabusiness@gmail.com>
2018-08-06 14:24:04 +0200

    Test again to implement it to the listener

--- Spotlight-library/src/main/java/com/wooplr/spotlight/SpotlightView.java
+++ Spotlight-library/src/main/java/com/wooplr/spotlight/SpotlightView.java
@@ -854,7 +854,7 @@ public class SpotlightView extends FrameLayout {
     private void removeSpotlightView() {
 
         if (listener != null)
-            listener.onUserClicked(usageId);
+            listener.onUserClicked(usageId, this);
 
         if (getParent() != null)
             ((ViewGroup) getParent()).removeView(this);
@@ -867,7 +867,7 @@ public class SpotlightView extends FrameLayout {
     public void removeSpotlightView(boolean needOnUserClickedCallback) {
         try{
             if(needOnUserClickedCallback && listener != null)
-                listener.onUserClicked(usageId);
+                listener.onUserClicked(usageId, this);
 
             if (getParent() != null)
                 ((ViewGroup) getParent()).removeView(this);
